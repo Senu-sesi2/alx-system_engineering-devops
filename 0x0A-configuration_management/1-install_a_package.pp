429d02c84adc0c2d65c4e8854eb3a356ed849e95
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-24 15:59:19 +0000

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-}
-
-exec { 'install_werkzeug':
-  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
-  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+# puppet declarative script to install flask from pip3.
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
