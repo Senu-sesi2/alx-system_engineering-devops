0da5d35d7a1a0a41c0bbcc6f3a342539c1c6bd0d
Senu-sesi2 <senu.sesi2@gmail.com>
2024-06-18 11:34:59 +0000

    updated files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
+Queries the Reddit API and prints the titles of the first 10 hot posts listed
+for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    prints the titles of the first 10 hot posts listed for
-    a given subreddit
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
-            print(posts.get('data').get('title'))
-
-    else:
-        print(None)
+    url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
+        subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
+    response = requests.get(url, headers=headers)
+    try:
+        top_ten = response.json()['data']['children']
+        for post in top_ten:
+            print(post['data']['title'])
+    except KeyError:
+        print("None")
